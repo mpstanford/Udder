da661cf6268293f926cc8d2e2ca0adb1ec775c7b
Madison Stanford-Geromel <madison@madisons-macbook-air.local>
2018-05-03 12:12:09 +0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Madison Stanford-Geromel on 5/3/18.
+//  Copyright © 2018 Madison Stanford-Geromel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
